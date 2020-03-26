e9e79f5b91a144f42b7523ed432cc870437553bc
yosefrow <yosefrow@gmail.com>
2020-03-26 23:57:16 +0200

    code refactor

--- how-log-does-it-last.py
+++ how-log-does-it-last.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python
+
 """ 
-Lookup expiration dates for common unlabeled foods
+Lookup expiration dates for unlabeled foods
+
+Powered by 
 """
 import requests
 import re
@@ -8,17 +11,17 @@ import sys
 
 import pandas
 
-def main(produce):  
-    print(f"Looking up: {produce}")
-    
-    try: 
-        from googlesearch import search 
-    except ImportError:  
-        print("No module named 'google' found") 
-      
+try: 
+    from googlesearch import search 
+except ImportError:  
+    print("No module named 'google' found") 
+
+SOURCE_SITE = "www.eatbydate.com"
+TABLE_REGEX_PATTERN = '<table id="unopened">(.|\n)*?</table>'
+
+def get_page_url(food, site):     
     # to search 
-    site = "www.eatbydate.com"
-    query = f"site:{site} how long do {produce} last"
+    query = f"site:{site} how long do {food} last"
       
     result = None
     url = None
@@ -26,35 +29,53 @@ def main(produce):
     for result in search(query, tld="com", num=1, stop=1, pause=2): 
         url = result
 
-    if url == None:
-        print(f"error! no result found for: {produce}")
-        exit(1)
-    
+    return url
+
+def get_page_html(url):
     response = requests.get(url)
-    text = response.text
+    html = response.text
     
-    if not (response.status_code == 200 and re.search('Error 404', text) == None):
-        print ('error! page lookup failed!')
-        exit(1)
+    if not (response.status_code == 200 and re.search('Error 404', html) == None):
+        html = None
     
-    print(url)
-    
-    table_html_pattern = '<table id="unopened">(.|\n)*?</table>' #'<table id="unopened">'
-    table_html_matches = re.search(table_html_pattern, text)
+    return html
+
+def extract_html_table(html, regex_pattern):
+    table_html_matches = re.search(regex_pattern, html)
 
     try:
-        table_html_string = table_html_matches[0]
+        table_html = table_html_matches[0]
     except TypeError:
-        print("warn: no matches found")
-        exit(1)
+        table_html = None
     pass
 
-    #print(table_html_string)
+    return table_html
 
-    table = pandas.read_html(table_html_string)
-    print(table)
+def pretty_print_table_html(table_html):
+    table = pandas.read_html(table_html)
+    print(table[0])
+
+def main(food):  
+    print(f"Looking up: '{food}' ...")
+
+    url = get_page_url(food, SOURCE_SITE)
+    if url == None:
+        print(f"error! no result found for '{food}'")
+        exit(1)
+    print(f"url: {url}")
+    
+    html = get_page_html(url)
+    if html == None:
+        print(f"error! page lookup failed for '{food}'")
+        exit(1)
+        
+    table_html = extract_html_table(html, TABLE_REGEX_PATTERN)
+    if table_html == None:
+        print(f"error! table not found for '{food}'")
+        exit(1)
+
+    pretty_print_table_html(table_html)
 
 if __name__ == '__main__':
-    produce = sys.argv[1]
-    main(produce)
-    
+    food = sys.argv[1]
+    main(food)
